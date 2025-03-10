110291ee3888a8dbb1d94abdc1d09a42b1f3f7c9
Sam ⌐◨-◨ <56174573+Sam-Scolari@users.noreply.github.com>
2025-03-10 14:27:36 -0500

    fix wasm

--- apps/website/app/api/images/wasm.ts
+++ apps/website/app/api/images/wasm.ts
@@ -1,11 +1,17 @@
 import { initWasm } from "@resvg/resvg-wasm";
+import path from "path";
+import fs from "fs";
 
 export let isInitialized = false;
 
 export async function init() {
 	if (isInitialized) return;
 
-	await initWasm("./resvg.wasm");
+	const wasmPath = path.join(process.cwd(), "public", "resvg.wasm");
+	console.log("WASM PATH", wasmPath);
+	const wasmBuffer = fs.readFileSync(wasmPath);
+
+	await initWasm(wasmBuffer);
 
 	isInitialized = true;
 }
