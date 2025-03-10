7d4f68498bfb7f794ee18df04c9e544b4c397d4d
Sam ⌐◨-◨ <56174573+Sam-Scolari@users.noreply.github.com>
2025-03-10 15:34:45 -0500

    Update generateImage.tsx

--- apps/website/app/api/images/generateImage.tsx
+++ apps/website/app/api/images/generateImage.tsx
@@ -2,10 +2,7 @@ import { initWasm } from "@resvg/resvg-wasm";
 import { unstable_cache as cache } from "next/cache";
 import path, { join } from "path";
 import fs from "fs";
-import { Resvg } from "@resvg/resvg-wasm";
 import satori from "satori";
-
-let isInitialized = false;
 
 export const generateImage = cache(
 	async (input: {
@@ -15,16 +12,6 @@ export const generateImage = cache(
 			height: number;
 		};
 	}) => {
-		if (!isInitialized) {
-			const wasmBuffer = fs.readFileSync(
-				path.join(process.cwd(), "public", "resvg.wasm"),
-			);
-
-			await initWasm(wasmBuffer);
-
-			isInitialized = true;
-		}
-
 		const Cabin = fs.readFileSync(
 			join(process.cwd(), "./public/fonts/Cabin-Regular.ttf"),
 		);
@@ -38,40 +25,36 @@ export const generateImage = cache(
 			join(process.cwd(), "./public/fonts/LondrinaSolid-Regular.ttf"),
 		);
 
-		return new Resvg(
-			await satori(input.image, {
-				width: input.size.width,
-				height: input.size.height,
-				fonts: [
-					{
-						name: "Cabin",
-						data: Cabin,
-						weight: 400,
-						style: "normal",
-					},
-					{
-						name: "Luckiest Guy",
-						data: LuckiestGuy,
-						weight: 400,
-						style: "normal",
-					},
-					{
-						name: "Bebas Neue",
-						data: BebasNeue,
-						weight: 400,
-						style: "normal",
-					},
-					{
-						name: "Londrina Solid",
-						data: LondrinaSolid,
-						weight: 400,
-						style: "normal",
-					},
-				],
-			}),
-		)
-			.render()
-			.asPng();
+		return satori(input.image, {
+			width: input.size.width,
+			height: input.size.height,
+			fonts: [
+				{
+					name: "Cabin",
+					data: Cabin,
+					weight: 400,
+					style: "normal",
+				},
+				{
+					name: "Luckiest Guy",
+					data: LuckiestGuy,
+					weight: 400,
+					style: "normal",
+				},
+				{
+					name: "Bebas Neue",
+					data: BebasNeue,
+					weight: 400,
+					style: "normal",
+				},
+				{
+					name: "Londrina Solid",
+					data: LondrinaSolid,
+					weight: 400,
+					style: "normal",
+				},
+			],
+		});
 	},
 	["generateImage"],
 	{
