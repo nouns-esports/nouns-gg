7355ba77e494575247de1cd4d2cdc730b3f6b469
Sam ⌐◨-◨ <56174573+Sam-Scolari@users.noreply.github.com>
2025-03-10 09:47:45 -0500

    Better error replies

--- packages/agent/plugins/discord.ts
+++ packages/agent/plugins/discord.ts
@@ -1,4 +1,3 @@
-import { ToolExecutionError } from "ai";
 import { createPlugin } from "../core/createPlugin";
 import { Client } from "discord.js";
 
@@ -16,29 +15,43 @@ export function discordPlugin(options: { token: string }) {
 
 			const mentioned = message.mentions.has(client.user.id);
 
+			const author = message.author.username.split("#")[0];
+			const mentions = message.mentions.users
+				.filter((user) => user.id !== client.user?.id)
+				.map((user) => user.username.split("#")[0]);
+			const room = message.channel.id;
+			const embeds: string[] = [];
+
 			if (mentioned) {
 				let response: string | undefined;
 
 				try {
 					const reply = await generateReply(message.content, {
 						id: message.id,
-						author: message.author.username.split("#")[0],
-						room: message.channel.id,
-						mentions: message.mentions.users
-							.filter((user) => user.id !== client.user?.id)
-							.map((user) => user.username.split("#")[0]),
-						embeds: [],
+						author,
+						room,
+						mentions,
+						embeds,
 					});
 
 					response = reply.text;
 				} catch (error) {
-					if (error instanceof ToolExecutionError) {
-						response = error.message;
-					} else if (error instanceof Error) {
-						response = error.message;
+					if (error instanceof Error) {
+						try {
+							const errorReply = await generateReply(error.message, {
+								id: message.id,
+								author,
+								room,
+								mentions,
+								embeds,
+							});
+							response = errorReply.text;
+						} catch (error) {
+							response =
+								"Sorry, something went wrong and I couldn't complete your task.";
+						}
 					} else {
-						response =
-							"Sorry, something went wrong and I couldn't complete your task.";
+						response = "Sorry, an unknown error occurred.";
 					}
 				}
 
