dcfaaf26b4ced77329172b0e102c6a52a0085e64
Sam ⌐◨-◨ <56174573+Sam-Scolari@users.noreply.github.com>
2025-03-10 16:13:16 -0500

    vercel/og

--- apps/website/app/api/images/votes/route.tsx
+++ apps/website/app/api/images/votes/route.tsx
@@ -1,6 +1,6 @@
 import { getUser } from "@/server/queries/users";
 import { getUserVotesForRound } from "@/server/queries/votes";
-import { generateImage } from "../generateImage";
+import { ImageResponse } from "@vercel/og";
 
 export async function GET(request: Request) {
 	const url = new URL(request.url);
@@ -39,172 +39,163 @@ export async function GET(request: Request) {
 		return Response.json({ error: "User has no votes" }, { status: 404 });
 	}
 
-	return new Response(
-		await generateImage({
-			size: {
-				width: 1200,
-				height: 800,
-			},
-			image: (
-				<div
-					style={{
-						color: "white",
-						backgroundColor: "#121213",
-						display: "flex",
-						flexDirection: "column",
-						justifyContent: "space-between",
-						height: "100%",
-						width: "100%",
-						position: "relative",
-						overflow: "hidden",
-					}}
-				>
-					{/* Background image with blur */}
-					<img
-						src={round.image}
-						style={{
-							position: "absolute",
-							width: "100%",
-							height: "100%",
-							objectFit: "cover",
-							filter: "blur(100px)",
-							opacity: 0.5,
-							transform: "scale(1.1)", // Prevents blur edges from showing
-						}}
-					/>
+	return new ImageResponse(
+		<div
+			style={{
+				color: "white",
+				backgroundColor: "#121213",
+				display: "flex",
+				flexDirection: "column",
+				justifyContent: "space-between",
+				height: "100%",
+				width: "100%",
+				position: "relative",
+				overflow: "hidden",
+			}}
+		>
+			{/* Background image with blur */}
+			<img
+				src={round.image}
+				style={{
+					position: "absolute",
+					width: "100%",
+					height: "100%",
+					objectFit: "cover",
+					filter: "blur(100px)",
+					opacity: 0.5,
+					transform: "scale(1.1)", // Prevents blur edges from showing
+				}}
+			/>
 
-					{/* Content on top with transparent background */}
+			{/* Content on top with transparent background */}
+			<div
+				style={{
+					display: "flex",
+					flexDirection: "column",
+					justifyContent: "space-between",
+					padding: 48,
+					flex: 1,
+					position: "relative", // Position on top of the background
+					backgroundColor: "transparent", // Semi-transparent dark background
+					zIndex: 1,
+					height: "100%",
+				}}
+			>
+				<div style={{ display: "flex", flexDirection: "column", gap: 48 }}>
+					<div
+						style={{
+							display: "flex",
+							flexDirection: "row",
+							gap: 32,
+							alignItems: "center",
+						}}
+					>
+						<img
+							src={user.image}
+							style={{ width: 90, height: 90, borderRadius: "100%" }}
+						/>
+						<div
+							style={{
+								display: "flex",
+								fontSize: 64,
+								fontWeight: 600,
+								fontFamily: "Bebas Neue",
+							}}
+						>
+							{user.name}'s Votes
+						</div>
+					</div>
 					<div
 						style={{
 							display: "flex",
 							flexDirection: "column",
-							justifyContent: "space-between",
-							padding: 48,
-							flex: 1,
-							position: "relative", // Position on top of the background
-							backgroundColor: "transparent", // Semi-transparent dark background
-							zIndex: 1,
-							height: "100%",
+							gap: 32,
 						}}
 					>
-						<div style={{ display: "flex", flexDirection: "column", gap: 48 }}>
-							<div
-								style={{
-									display: "flex",
-									flexDirection: "row",
-									gap: 32,
-									alignItems: "center",
-								}}
-							>
-								<img
-									src={user.image}
-									style={{ width: 90, height: 90, borderRadius: "100%" }}
-								/>
+						{round.votes
+							.slice(0, round.votes.length > 4 ? 3 : 4)
+							.map((vote) => (
 								<div
+									key={vote.proposal.title}
 									style={{
 										display: "flex",
-										fontSize: 64,
-										fontWeight: 600,
-										fontFamily: "Bebas Neue",
+										justifyContent: "space-between",
+										gap: 64,
+										width: "100%",
+										backgroundColor: "rgba(255, 255, 255, 0.15)", // More transparent
+										paddingLeft: 32,
+										paddingRight: 32,
+										paddingTop: 20,
+										paddingBottom: 20,
+										borderRadius: 16,
 									}}
 								>
-									{user.name}'s Votes
-								</div>
-							</div>
-							<div
-								style={{
-									display: "flex",
-									flexDirection: "column",
-									gap: 32,
-								}}
-							>
-								{round.votes
-									.slice(0, round.votes.length > 4 ? 3 : 4)
-									.map((vote) => (
-										<div
-											key={vote.proposal.title}
-											style={{
-												display: "flex",
-												justifyContent: "space-between",
-												gap: 64,
-												width: "100%",
-												backgroundColor: "rgba(255, 255, 255, 0.15)", // More transparent
-												paddingLeft: 32,
-												paddingRight: 32,
-												paddingTop: 20,
-												paddingBottom: 20,
-												borderRadius: 16,
-											}}
-										>
-											<div
-												style={{
-													display: "block",
-													lineClamp: 1,
-													fontSize: 36,
-													fontFamily: "Cabin",
-												}}
-											>
-												{vote.proposal.title.replace(
-													/[^a-zA-Z0-9 \-_\!\@\#\$\%\^\&\*\(\)\+\=\"\'\?\/\>\<,\.\{\}\[\]\|\\\~\`\;\:\n\r\t]/g,
-													"",
-												)}
-											</div>
-											<div
-												style={{
-													display: "flex",
-													gap: 16,
-													alignItems: "center",
-												}}
-											>
-												<div
-													style={{
-														display: "flex",
-														fontSize: 36,
-														fontFamily: "Cabin",
-													}}
-												>
-													{vote.count.toString()}
-												</div>
-											</div>
-										</div>
-									))}
-								{round.votes.length > 4 ? (
 									<div
 										style={{
-											display: "flex",
+											display: "block",
+											lineClamp: 1,
 											fontSize: 36,
 											fontFamily: "Cabin",
 										}}
 									>
-										+{(round.votes.length - 4).toString()} more
+										{vote.proposal.title.replace(
+											/[^a-zA-Z0-9 \-_\!\@\#\$\%\^\&\*\(\)\+\=\"\'\?\/\>\<,\.\{\}\[\]\|\\\~\`\;\:\n\r\t]/g,
+											"",
+										)}
 									</div>
-								) : (
-									""
-								)}
-							</div>
-						</div>
-						{round.votes.length < 5 ? (
+									<div
+										style={{
+											display: "flex",
+											gap: 16,
+											alignItems: "center",
+										}}
+									>
+										<div
+											style={{
+												display: "flex",
+												fontSize: 36,
+												fontFamily: "Cabin",
+											}}
+										>
+											{vote.count.toString()}
+										</div>
+									</div>
+								</div>
+							))}
+						{round.votes.length > 4 ? (
 							<div
 								style={{
-									fontSize: 36,
 									display: "flex",
-									color: "white",
+									fontSize: 36,
 									fontFamily: "Cabin",
 								}}
 							>
-								nouns.gg/rounds/{round.id}
+								+{(round.votes.length - 4).toString()} more
 							</div>
 						) : (
 							""
 						)}
 					</div>
 				</div>
-			),
-		}),
+				{round.votes.length < 5 ? (
+					<div
+						style={{
+							fontSize: 36,
+							display: "flex",
+							color: "white",
+							fontFamily: "Cabin",
+						}}
+					>
+						nouns.gg/rounds/{round.id}
+					</div>
+				) : (
+					""
+				)}
+			</div>
+		</div>,
 		{
-			headers: {
-				"Content-Type": "image/svg+xml",
-			},
+			width: 1200,
+			height: 800,
 		},
 	);
 }
