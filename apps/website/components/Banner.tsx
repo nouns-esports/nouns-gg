aa66697fe602cecf4a43e8af1c1df0dfe3b13cc2
Sam ⌐◨-◨ <56174573+Sam-Scolari@users.noreply.github.com>
2025-03-14 17:09:11 -0500

    Update Banner.tsx

--- apps/website/components/Banner.tsx
+++ apps/website/components/Banner.tsx
@@ -5,19 +5,19 @@ import Link from "@/components/Link";
 import { usePathname } from "next/navigation";
 
 export default function Banner() {
-  const pathname = usePathname();
+	const pathname = usePathname();
 
-  if (pathname === "/") {
-    return (
-      <Link
-        href="/rounds"
-        className="bg-red relative z-[70] h-9 flex-shrink-0 hover:brightness-[85%] transition-all text-white text-sm font-semibold w-full whitespace-nowrap flex items-center justify-center"
-      >
-        <div className="flex items-center justify-center overflow-hidden">
-          Explore rounds, vote for your favorite proposals, and more!
-          <ArrowRight className="w-4 h-4 ml-1.5" />
-        </div>
-      </Link>
-    );
-  }
+	if (pathname === "/") {
+		return (
+			<Link
+				href="/leaderboard"
+				className="bg-red relative z-[70] h-9 flex-shrink-0 hover:brightness-[85%] transition-all text-white text-sm font-semibold w-full whitespace-nowrap flex items-center justify-center"
+			>
+				<div className="flex items-center justify-center overflow-hidden">
+					Rank up and earn rewards!
+					<ArrowRight className="w-4 h-4 ml-1.5" />
+				</div>
+			</Link>
+		);
+	}
 }
