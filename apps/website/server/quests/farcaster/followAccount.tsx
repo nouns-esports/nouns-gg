97c49cbe44afc403652d65cc625af96bd53a6c11
Sam ⌐◨-◨ <56174573+Sam-Scolari@users.noreply.github.com>
2025-03-21 14:29:40 -0500

    Update followAccount.tsx

--- apps/website/server/quests/farcaster/followAccount.tsx
+++ apps/website/server/quests/farcaster/followAccount.tsx
@@ -2,36 +2,44 @@ import { neynarClient } from "@/server/clients/neynar";
 import createAction from "../createAction";
 
 export const followAccount = createAction<{ account?: string }>(
-  async (actionInputs) => {
-    if (!actionInputs.account) {
-      throw new Error("Account input missing in action");
-    }
+	async (actionInputs) => {
+		if (!actionInputs.account) {
+			throw new Error("Account input missing in action");
+		}
 
-    return {
-      description: (
-        <p>
-          Follow <span className="text-red">@{actionInputs.account}</span>
-        </p>
-      ),
-      url: `https://warpcast.com/${actionInputs.account}`,
-      check: async (user) => {
-        if (!actionInputs.account) return false;
-        if (!user.farcaster) return false;
+		return {
+			description: (
+				<p>
+					Follow <span className="text-red">@{actionInputs.account}</span>
+				</p>
+			),
+			url: `https://warpcast.com/${actionInputs.account}`,
+			check: async (user) => {
+				if (!actionInputs.account) return false;
+				if (!user.farcaster) return false;
 
-        const response = await neynarClient.searchUser(
-          actionInputs.account,
-          user.farcaster.fid,
-          { limit: 1 }
-        );
+				const response = await neynarClient.searchUser(
+					actionInputs.account,
+					user.farcaster.fid,
+					{ limit: 1 },
+				);
 
-        if (response.result.users?.[0].username !== actionInputs.account) {
-          return false;
-        }
+				console.log("account response", response);
 
-        if (!response.result.users?.[0].viewer_context?.following) return false;
+				if (response.result.users.length === 0) {
+					console.log("User not found");
+					return false;
+				}
 
-        return true;
-      },
-    };
-  }
+				if (response.result.users[0]?.username !== actionInputs.account) {
+					console.log("Username mismatch");
+					return false;
+				}
+
+				if (!response.result.users[0].viewer_context?.following) return false;
+
+				return true;
+			},
+		};
+	},
 );
