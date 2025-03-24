3138d550313c02748feb179b5d595953e7286792
Sam ⌐◨-◨ <56174573+Sam-Scolari@users.noreply.github.com>
2025-03-24 13:50:12 -0500

    Update estimateTimestamp.ts

--- packages/utils/estimateTimestamp.ts
+++ packages/utils/estimateTimestamp.ts
@@ -3,5 +3,13 @@ export function estimateTimestamp(props: {
 	now?: Date;
 }) {
 	const now = props.now ?? new Date();
+
+	console.log(
+		"ESTIMATING TIMESTAMP",
+		props.blocks,
+		now.getTime(),
+		new Date(now.getTime() + props.blocks * 12 * 1000),
+	);
+
 	return new Date(now.getTime() + props.blocks * 12 * 1000);
 }
