11c86cfa153701559e0f8d10317eea56f77769e6
Sam ⌐◨-◨ <56174573+Sam-Scolari@users.noreply.github.com>
2025-03-25 20:20:31 -0500

    Update communities.ts

--- apps/website/server/queries/communities.ts
+++ apps/website/server/queries/communities.ts
@@ -1,4 +1,4 @@
-import { communities, rosters, talent } from "~/packages/db/schema/public";
+import { communities } from "~/packages/db/schema/public";
 import { db } from "~/packages/db";
 import { and, asc, eq, inArray, isNull } from "drizzle-orm";
 import { unstable_cache as cache } from "next/cache";
@@ -6,14 +6,8 @@ import { unstable_cache as cache } from "next/cache";
 export const getCommunities = cache(
 	async (input?: { ids?: string[] }) => {
 		return db.pgpool.query.communities.findMany({
-			where: and(
-				input?.ids ? inArray(communities.id, input.ids) : undefined,
-				isNull(communities.parent),
-			),
+			where: and(input?.ids ? inArray(communities.id, input.ids) : undefined),
 			orderBy: asc(communities.name),
-			with: {
-				children: true,
-			},
 		});
 	},
 	["getCommunities"],
@@ -24,30 +18,8 @@ export const getCommunity = cache(
 	async (input: { id: string }) => {
 		return db.pgpool.query.communities.findFirst({
 			where: eq(communities.id, input.id),
-			with: {
-				children: true,
-			},
 		});
 	},
 	["getCommunity"],
-	{ revalidate: 60 * 10 },
-);
-
-export const getCommunityRosters = cache(
-	async (input: { community: string }) => {
-		return db.pgpool.query.rosters.findMany({
-			where: and(
-				eq(rosters.community, input.community),
-				eq(rosters.active, true),
-			),
-			with: {
-				community: true,
-				talent: {
-					where: eq(talent.active, true),
-				},
-			},
-		});
-	},
-	["getCommunityRosters"],
 	{ revalidate: 60 * 10 },
 );
