c24d8807790c9e4bb1a30ce47fdbeaba4dbc20db
Sam <samscolari@icloud.com>
2025-04-05 22:00:28 -0500

    Update page.tsx

--- apps/website/app/test/page.tsx
+++ apps/website/app/test/page.tsx
@@ -1,26 +1,79 @@
 import { db } from "~/packages/db";
 
 export default async function Test() {
-	let error = "";
+	let test1 = "";
+	let test2 = "";
+	let test3 = "";
+	let test4 = "";
+	let test5 = "";
 
 	try {
 		const test = await db.pgpool.query.nouns.findMany({
 			limit: 10,
 		});
+
+		test1 = JSON.stringify(test);
 	} catch (e) {
 		if (e instanceof Error) {
-			error = e.message;
+			test1 = e.message;
 		}
 	}
 
-	const test = await db.pgpool.query.nounDelegates.findMany({
-		limit: 10,
-	});
+	try {
+		const test = await db.pgpool.query.nounDelegates.findMany({
+			limit: 10,
+		});
+
+		test2 = JSON.stringify(test);
+	} catch (e) {
+		if (e instanceof Error) {
+			test2 = e.message;
+		}
+	}
+
+	try {
+		const test = await db.pgpool.query.erc721Balances.findMany({
+			limit: 10,
+		});
+
+		test3 = JSON.stringify(test);
+	} catch (e) {
+		if (e instanceof Error) {
+			test3 = e.message;
+		}
+	}
+
+	try {
+		const test = await db.pgpool.query.nounsVotes.findMany({
+			limit: 10,
+		});
+
+		test4 = JSON.stringify(test);
+	} catch (e) {
+		if (e instanceof Error) {
+			test4 = e.message;
+		}
+	}
+
+	try {
+		const test = await db.pgpool.query.nounsBids.findMany({
+			limit: 10,
+		});
+
+		test5 = JSON.stringify(test);
+	} catch (e) {
+		if (e instanceof Error) {
+			test5 = e.message;
+		}
+	}
 
 	return (
-		<div className="px-32 pt-64">
-			{error}
-			{JSON.stringify(test)}
+		<div className="px-32 pt-64 flex flex-col gap-4 text-white">
+			<p>nouns: {test1}</p>
+			<p>nounDelegates: {test2}</p>
+			<p>erc721Balances: {test3}</p>
+			<p>nounsVotes: {test4}</p>
+			<p>nounsBids: {test5}</p>
 		</div>
 	);
 }
