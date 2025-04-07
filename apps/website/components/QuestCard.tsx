1a827b831193969e5a2a9727e755ad39553cf227
Sam <samscolari@icloud.com>
2025-04-07 13:09:55 -0500

    Update QuestCard.tsx

--- apps/website/components/QuestCard.tsx
+++ apps/website/components/QuestCard.tsx
@@ -57,7 +57,7 @@ export default function QuestCard(props: {
 					) : null}
 					{props.quest.creator ? (
 						<Link
-							href={`/users/${event.creator.username ?? event.creator.id}`}
+							href={`/users/${props.quest.creator.username ?? props.quest.creator.id}`}
 							newTab
 							className="relative z-20 bg-grey-500 hover:bg-grey-400 transition-colors py-2 pl-2 pr-3 rounded-full flex text-white items-center gap-2 text-sm font-semibold w-fit whitespace-nowrap"
 						>
