2d7b21d65e90af4520d8e3aae5f6d8eee010950b
Sam <56174573+Sam-Scolari@users.noreply.github.com>
2025-04-07 12:52:12 -0500

    Update RoundCard.tsx

--- apps/website/components/RoundCard.tsx
+++ apps/website/components/RoundCard.tsx
@@ -89,7 +89,7 @@ export default function RoundCard(props: {
 				{props.round.creator ? (
 					<div className="flex h-full items-end">
 						<Link
-							href={`/users/${props.round.creator}`}
+							href={`/users/${event.creator.username ?? event.creator.id}`}
 							className="relative z-20 bg-grey-500 hover:bg-grey-400 transition-colors py-2 pl-2 pr-3 rounded-full flex text-white items-center gap-2 text-sm font-semibold w-fit"
 						>
 							<img
