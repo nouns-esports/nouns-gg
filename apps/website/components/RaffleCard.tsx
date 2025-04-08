b8ab9ca42876d308fdc81cf843167946e520b8a9
Sam <samscolari@icloud.com>
2025-04-07 19:02:18 -0500

    Update RaffleCard.tsx

--- apps/website/components/RaffleCard.tsx
+++ apps/website/components/RaffleCard.tsx
@@ -4,9 +4,10 @@ import Button from "./Button";
 import { Clock, User } from "lucide-react";
 import Countdown from "./Countdown";
 
-export default function ProductCard(props: {
+export default function RaffleCard(props: {
 	raffle: NonNullable<Awaited<ReturnType<typeof getRaffles>>>[number];
 }) {
+	console.log("raffle", props.raffle);
 	const userEntries = props.raffle.entries.reduce(
 		(acc, curr) => acc + curr.amount,
 		0,
