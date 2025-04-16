3bce2958a6311223859d5fe2ae2c4bed4ff2a143
Sam <samscolari@icloud.com>
2025-04-16 14:40:28 -0500

    Update page.tsx

--- apps/website/app/c/[community]/page.tsx
+++ apps/website/app/c/[community]/page.tsx
@@ -15,6 +15,7 @@ import { getLeaderboard, getRank } from "@/server/queries/leaderboard";
 import { level } from "@/utils/level";
 import { ToggleModal } from "@/components/Modal";
 import { Info } from "lucide-react";
+import RankingSystemExplainer from "@/components/modals/RankingSystemExplainer";
 
 export default async function Community(props: {
 	params: Promise<{ community: string }>;
@@ -65,138 +66,141 @@ export default async function Community(props: {
 		]);
 
 	return (
-		<div className="flex flex-col w-full items-center">
-			<div className="relative flex flex-col justify-center gap-8 w-full pt-32 max-xl:pt-28 max-sm:pt-20 px-32 max-2xl:px-16 max-xl:px-8 max-sm:px-4 max-w-[1920px]">
-				<div className="px-4 pt-4 flex flex-col gap-4 bg-grey-800 rounded-xl">
-					<div className="flex items-center gap-4">
-						<img
-							src={community.image}
-							alt={community.name}
-							className=" h-12 w-12 object-cover object-center rounded-md"
-						/>
-						<h1 className="text-white text-2xl font-luckiest-guy">
-							{community.name}
-						</h1>
+		<>
+			<div className="flex flex-col w-full items-center">
+				<div className="relative flex flex-col justify-center gap-8 w-full pt-32 max-xl:pt-28 max-sm:pt-20 px-32 max-2xl:px-16 max-xl:px-8 max-sm:px-4 max-w-[1920px]">
+					<div className="px-4 pt-4 flex flex-col gap-4 bg-grey-800 rounded-xl">
+						<div className="flex items-center gap-4">
+							<img
+								src={community.image}
+								alt={community.name}
+								className=" h-12 w-12 object-cover object-center rounded-md"
+							/>
+							<h1 className="text-white text-2xl font-luckiest-guy">
+								{community.name}
+							</h1>
+						</div>
+						{tab ? (
+							<ul className="flex gap-2 w-full overflow-x-auto">
+								{community.hasRounds ? (
+									<Tab href="?tab=rounds" active={tab === "rounds"}>
+										Rounds
+									</Tab>
+								) : null}
+								{community.hasQuests ? (
+									<Tab href="?tab=quests" active={tab === "quests"}>
+										Quests
+									</Tab>
+								) : null}
+								{community.hasPredictions ? (
+									<Tab href="?tab=predictions" active={tab === "predictions"}>
+										Predictions
+									</Tab>
+								) : null}
+								{community.hasEvents ? (
+									<Tab href="?tab=events" active={tab === "events"}>
+										Events
+									</Tab>
+								) : null}
+								{community.handle === "nouns-gg" ? (
+									<Tab href="?tab=leaderboard" active={tab === "leaderboard"}>
+										Leaderboard
+									</Tab>
+								) : null}
+							</ul>
+						) : null}
 					</div>
-					{tab ? (
-						<ul className="flex gap-2 w-full overflow-x-auto">
-							{community.hasRounds ? (
-								<Tab href="?tab=rounds" active={tab === "rounds"}>
-									Rounds
-								</Tab>
-							) : null}
-							{community.hasQuests ? (
-								<Tab href="?tab=quests" active={tab === "quests"}>
-									Quests
-								</Tab>
-							) : null}
-							{community.hasPredictions ? (
-								<Tab href="?tab=predictions" active={tab === "predictions"}>
-									Predictions
-								</Tab>
-							) : null}
-							{community.hasEvents ? (
-								<Tab href="?tab=events" active={tab === "events"}>
-									Events
-								</Tab>
-							) : null}
-							{community.handle === "nouns-gg" ? (
-								<Tab href="?tab=leaderboard" active={tab === "leaderboard"}>
-									Leaderboard
-								</Tab>
-							) : null}
-						</ul>
-					) : null}
-				</div>
-				<div className="flex flex-col gap-8 w-full">
-					{
+					<div className="flex flex-col gap-8 w-full">
 						{
-							events: (
-								<div className="grid grid-cols-3 max-xl:grid-cols-2 max-md:grid-cols-1  gap-4">
-									{events.map((event) => (
-										<EventCard key={`event-${event.id}`} event={event} />
-									))}
-								</div>
-							),
-							rounds: (
-								<div className="grid grid-cols-4 max-xl:grid-cols-3 max-lg:grid-cols-2 max-sm:grid-cols-1 gap-4">
-									{rounds.map((round) => (
-										<RoundCard key={`round-${round.id}`} round={round} />
-									))}
-								</div>
-							),
-							quests: (
-								<div className="grid grid-cols-5 max-2xl:grid-cols-4 max-lg:grid-cols-3 max-md:grid-cols-2 max-sm:grid-cols-1 gap-4">
-									{quests.map((quest) => (
-										<QuestCard key={`quest-${quest.id}`} quest={quest} />
-									))}
-								</div>
-							),
-							predictions: (
-								<div className="flex flex-col gap-6">
-									<h2 className="text-white font-luckiest-guy text-2xl">
-										Happening Now
-									</h2>
-									<div className="grid grid-cols-4 max-2xl:grid-cols-3 max-lg:grid-cols-2 max-md:flex max-md:flex-col gap-4">
-										{predictions.map((prediction) => (
-											<PredictionCard
-												key={`prediction-${prediction.id}`}
-												prediction={prediction}
-												className="max-md:w-full max-md:flex-shrink-0"
-											/>
+							{
+								events: (
+									<div className="grid grid-cols-3 max-xl:grid-cols-2 max-md:grid-cols-1  gap-4">
+										{events.map((event) => (
+											<EventCard key={`event-${event.id}`} event={event} />
 										))}
 									</div>
-								</div>
-							),
-							leaderboard: (
-								<div className="flex flex-col items-center gap-4">
-									<div className="relative flex flex-col gap-8 max-w-2xl w-full">
-										{userRank && user?.nexus ? (
-											<div className="flex flex-col gap-2">
-												<p className="text-white text-lg font-semibold">
-													Your Position
-												</p>
-												<LeaderboardPosition
-													key={user?.id}
-													position={userRank}
-													user={user.nexus}
+								),
+								rounds: (
+									<div className="grid grid-cols-4 max-xl:grid-cols-3 max-lg:grid-cols-2 max-sm:grid-cols-1 gap-4">
+										{rounds.map((round) => (
+											<RoundCard key={`round-${round.id}`} round={round} />
+										))}
+									</div>
+								),
+								quests: (
+									<div className="grid grid-cols-5 max-2xl:grid-cols-4 max-lg:grid-cols-3 max-md:grid-cols-2 max-sm:grid-cols-1 gap-4">
+										{quests.map((quest) => (
+											<QuestCard key={`quest-${quest.id}`} quest={quest} />
+										))}
+									</div>
+								),
+								predictions: (
+									<div className="flex flex-col gap-6">
+										<h2 className="text-white font-luckiest-guy text-2xl">
+											Happening Now
+										</h2>
+										<div className="grid grid-cols-4 max-2xl:grid-cols-3 max-lg:grid-cols-2 max-md:flex max-md:flex-col gap-4">
+											{predictions.map((prediction) => (
+												<PredictionCard
+													key={`prediction-${prediction.id}`}
+													prediction={prediction}
+													className="max-md:w-full max-md:flex-shrink-0"
 												/>
-											</div>
-										) : null}
-										<div className="flex flex-col gap-2">
-											<div className="flex gap-2 items-center justify-between">
-												<p className="text-white text-lg font-semibold">
-													All Time
-												</p>
-												<ToggleModal
-													id="ranking-system-explainer"
-													className="flex items-center gap-1.5 text-red hover:text-red/70 transition-colors"
-												>
-													<Info className="w-4 h-4" />
-													How do I earn XP?
-												</ToggleModal>
-											</div>
-											{leaderboard.map((user, index) => {
-												const position = index + 1;
-
-												return (
-													<LeaderboardPosition
-														key={user.id}
-														position={position}
-														user={user}
-													/>
-												);
-											})}
+											))}
 										</div>
 									</div>
-								</div>
-							),
-							default: null,
-						}[tab ?? "default"]
-					}
+								),
+								leaderboard: (
+									<div className="flex flex-col items-center gap-4">
+										<div className="relative flex flex-col gap-8 max-w-2xl w-full">
+											{userRank && user?.nexus ? (
+												<div className="flex flex-col gap-2">
+													<p className="text-white text-lg font-semibold">
+														Your Position
+													</p>
+													<LeaderboardPosition
+														key={user?.id}
+														position={userRank}
+														user={user.nexus}
+													/>
+												</div>
+											) : null}
+											<div className="flex flex-col gap-2">
+												<div className="flex gap-2 items-center justify-between">
+													<p className="text-white text-lg font-semibold">
+														All Time
+													</p>
+													<ToggleModal
+														id="ranking-system-explainer"
+														className="flex items-center gap-1.5 text-red hover:text-red/70 transition-colors"
+													>
+														<Info className="w-4 h-4" />
+														How do I earn XP?
+													</ToggleModal>
+												</div>
+												{leaderboard.map((user, index) => {
+													const position = index + 1;
+
+													return (
+														<LeaderboardPosition
+															key={user.id}
+															position={position}
+															user={user}
+														/>
+													);
+												})}
+											</div>
+										</div>
+									</div>
+								),
+								default: null,
+							}[tab ?? "default"]
+						}
+					</div>
 				</div>
 			</div>
-		</div>
+			{tab === "leaderboard" ? <RankingSystemExplainer /> : null}
+		</>
 	);
 }
 
