0458685b33cca34467d8fbb13fb474093add1c26
Sam <samscolari@icloud.com>
2025-05-05 09:59:26 -0400

    Update index.tsx

--- apps/website/providers/index.tsx
+++ apps/website/providers/index.tsx
@@ -3,56 +3,20 @@
 import Privy from "@/providers/Privy";
 import { Toaster } from "react-hot-toast";
 import ReactQuery from "./ReactQuery";
-// import Wagmi from "./Wagmi";
-import { usePathname } from "next/navigation";
-import { useEffect } from "react";
-import { create } from "zustand";
 import Farcaster from "./Farcaster";
 import PostHog from "./PostHog";
-
-const useFromExternalStore = create<{
-	initialPath?: string;
-	recentPath?: string;
-	setInitialPath: (path: string) => void;
-	setRecentPath: (path: string) => void;
-}>((set) => ({
-	initialPath: undefined,
-	recentPath: undefined,
-	setInitialPath: (path) => set({ initialPath: path }),
-	setRecentPath: (path) => set({ recentPath: path }),
-}));
-
-export function useFromExternal() {
-	const pathname = usePathname();
-	const { initialPath, recentPath, setInitialPath, setRecentPath } =
-		useFromExternalStore();
-
-	useEffect(() => {
-		if (!initialPath) {
-			setInitialPath(pathname);
-		} else if (initialPath !== pathname) {
-			setRecentPath(pathname);
-		}
-	}, [pathname, initialPath, setInitialPath, setRecentPath]);
-
-	return !recentPath;
-}
 
 export default function Providers(props: {
 	user?: string;
 	children: React.ReactNode;
 }) {
-	useFromExternal();
-
 	return (
 		<Privy user={props.user}>
 			<Farcaster>
 				<ReactQuery>
 					<PostHog>
-						{/* <Wagmi> */}
 						{props.children}
 						<Toaster position="top-center" />
-						{/* </Wagmi> */}
 					</PostHog>
 				</ReactQuery>
 			</Farcaster>
