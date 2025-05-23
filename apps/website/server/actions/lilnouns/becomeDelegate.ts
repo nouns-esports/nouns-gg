15ef2c1e84b01996a53c5dd9555fa0e214618932
Sam <samscolari@icloud.com>
2025-05-23 11:22:36 -0500

    fix

--- apps/website/server/actions/lilnouns/becomeDelegate.ts
+++ apps/website/server/actions/lilnouns/becomeDelegate.ts
@@ -1,7 +1,7 @@
 import { createAction } from "../createAction";
 import { db } from "~/packages/db";
-import { inArray } from "drizzle-orm";
-import { lilnounDelegates } from "~/packages/db/schema/indexer";
+import { and, eq, inArray } from "drizzle-orm";
+import { erc721Balances, lilnounDelegates } from "~/packages/db/schema/indexer";
 
 export const becomeLilNounsDelegate = createAction({
 	image: "",
@@ -28,14 +28,24 @@ export const becomeLilNounsDelegate = createAction({
 
 		if (user.wallets.length === 0) return false;
 
-		const isDelegate = await db.primary.query.lilnounDelegates.findFirst({
-			where: inArray(
-				lilnounDelegates.to,
-				user.wallets.map((w) => w.address.toLowerCase() as `0x${string}`),
-			),
-		});
+		const [isDelegate, isHolder] = await Promise.all([
+			db.primary.query.lilnounDelegates.findFirst({
+				where: inArray(
+					lilnounDelegates.to,
+					user.wallets.map((w) => w.address.toLowerCase() as `0x${string}`),
+				),
+			}),
+			db.primary.query.erc721Balances.findFirst({
+				where: and(inArray(
+					erc721Balances.account,
+					user.wallets.map((w) => w.address.toLowerCase() as `0x${string}`),
+				),
+					eq(erc721Balances.collection, "0x4b10701bfd7bfedc47d50562b76b436fbb5bdb3b"),
+				),
+			}),
+		]);
 
-		return !!isDelegate;
+		return !!isDelegate || !!isHolder;
 	},
 	filters: {},
 });
--- apps/website/server/actions/nouns/becomeDelegate.ts
+++ apps/website/server/actions/nouns/becomeDelegate.ts
@@ -1,7 +1,7 @@
 import { createAction } from "../createAction";
 import { db } from "~/packages/db";
-import { inArray } from "drizzle-orm";
-import { nounDelegates } from "~/packages/db/schema/indexer";
+import { and, eq, inArray } from "drizzle-orm";
+import { erc721Balances, nounDelegates } from "~/packages/db/schema/indexer";
 
 export const becomeNounsDelegate = createAction({
 	image: "",
@@ -28,14 +28,24 @@ export const becomeNounsDelegate = createAction({
 
 		if (user.wallets.length === 0) return false;
 
-		const isDelegate = await db.primary.query.nounDelegates.findFirst({
-			where: inArray(
-				nounDelegates.to,
-				user.wallets.map((w) => w.address.toLowerCase() as `0x${string}`),
-			),
-		});
+		const [isDelegate, isHolder] = await Promise.all([
+			db.primary.query.nounDelegates.findFirst({
+				where: inArray(
+					nounDelegates.to,
+					user.wallets.map((w) => w.address.toLowerCase() as `0x${string}`),
+				),
+			}),
+			db.primary.query.erc721Balances.findFirst({
+				where: and(inArray(
+					erc721Balances.account,
+					user.wallets.map((w) => w.address.toLowerCase() as `0x${string}`),
+				),
+					eq(erc721Balances.collection, "0x9c8ff314c9bc7f6e59a9d9225fb22946427edc03"),
+				),
+			}),
+		]);
 
-		return !!isDelegate;
+		return !!isDelegate || !!isHolder;
 	},
 	filters: {},
 });
