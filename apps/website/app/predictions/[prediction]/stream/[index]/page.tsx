dfa82508722452e72250a4f6742eefa1a5df116c
Sam <samscolari@icloud.com>
2025-05-30 13:43:36 -0500

    Update page.tsx

--- apps/website/app/predictions/[prediction]/stream/[index]/page.tsx
+++ apps/website/app/predictions/[prediction]/stream/[index]/page.tsx
@@ -56,42 +56,52 @@ export default async function StreamPage(props: {
 	const binary = outcomes.length === 2;
 
 	return (
-		<div className="z-[1000000] bg-[#0F0F0F] fixed w-screen h-screen overflow-hidden scrollbar-hidden text-white">
-			{binary ? (
-				<>
-					<div className="absolute bottom-0 left-0 w-full flex flex-col items-center justify-end h-full">
-						<div
-							className="w-full bg-gold-500"
-							style={{
-								height: `${odds === 0 ? 1 : odds === 100 ? 99 : odds}%`,
-							}}
-						/>
-					</div>
-					<div className="absolute bottom-[10vh] left-0 w-full flex flex-col items-center justify-end h-full">
+		<>
+			<style
+				// biome-ignore lint/security/noDangerouslySetInnerHtml: <explanation>
+				dangerouslySetInnerHTML={{
+					__html: `
+			  html, body { overflow: hidden !important; }
+			`,
+				}}
+			/>
+			<div className="z-[1000000] bg-[#0F0F0F] fixed w-screen h-screen overflow-hidden scrollbar-hidden text-white">
+				{binary ? (
+					<>
+						<div className="absolute bottom-0 left-0 w-full flex flex-col items-center justify-end h-full">
+							<div
+								className="w-full bg-gold-500"
+								style={{
+									height: `${odds === 0 ? 1 : odds === 100 ? 99 : odds}%`,
+								}}
+							/>
+						</div>
+						<div className="absolute bottom-[10vh] left-0 w-full flex flex-col items-center justify-end h-full">
+							<p
+								className="font-londrina-solid"
+								style={{ fontSize: "30vw", lineHeight: 1 }}
+							>
+								{odds === 0 ? 1 : odds === 100 ? 99 : odds}%
+							</p>
+						</div>
+					</>
+				) : (
+					<div className="w-full h-full flex items-center justify-between px-[20vh]">
 						<p
-							className="font-londrina-solid"
-							style={{ fontSize: "30vw", lineHeight: 1 }}
+							className="font-londrina-solid text-nowrap text-[#FFFFFF]"
+							style={{ fontSize: "30vh", lineHeight: 1 }}
+						>
+							{outcome.name}
+						</p>
+						<p
+							className="font-londrina-solid text-[#FED439]"
+							style={{ fontSize: "30vh", lineHeight: 1 }}
 						>
 							{odds === 0 ? 1 : odds === 100 ? 99 : odds}%
 						</p>
 					</div>
-				</>
-			) : (
-				<div className="w-full h-full flex items-center justify-between px-[20vh]">
-					<p
-						className="font-londrina-solid text-nowrap text-[#FFFFFF]"
-						style={{ fontSize: "30vh", lineHeight: 1 }}
-					>
-						{outcome.name}
-					</p>
-					<p
-						className="font-londrina-solid text-[#FED439]"
-						style={{ fontSize: "30vh", lineHeight: 1 }}
-					>
-						{odds === 0 ? 1 : odds === 100 ? 99 : odds}%
-					</p>
-				</div>
-			)}
-		</div>
+				)}
+			</div>
+		</>
 	);
 }
