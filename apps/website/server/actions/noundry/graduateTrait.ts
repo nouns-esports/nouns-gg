9eecdfe47ee1de8b3f48e7082f50e477acd219ab
Sam <samscolari@icloud.com>
2025-06-01 18:56:27 -0500

    noundry actions

---
+++ apps/website/server/actions/noundry/graduateTrait.ts
@@ -0,0 +1,73 @@
+import { z } from "zod";
+import { createAction } from "../createAction";
+import { createFilter } from "../createFilter";
+
+export const graduateTrait = createAction({
+    image: "",
+    name: "Graduate Trait",
+    category: "noundry",
+    generateDescription: async (inputs) => {
+        "use server";
+
+        const parts = [];
+
+        if (inputs.count.value > 1) {
+            parts.push({
+                text: "Graduate at least",
+                highlight: true,
+            });
+            parts.push({
+                text: inputs.count.value.toString(),
+                highlight: true,
+            });
+            parts.push({
+                text: "traits from",
+            });
+        } else {
+            parts.push({
+                text: "Graduate a trait from",
+            });
+        }
+
+        parts.push({
+            text: "Noundry",
+            href: "https://noundry.wtf",
+        });
+
+        parts.push({
+            text: "to Nouns",
+        });
+
+        return parts;
+    },
+    check: async ({ user, inputs }) => {
+        "use server";
+
+        if (user.wallets.length === 0) return false;
+
+        const graduates = await fetch("https://gallery.noundry.wtf/api/graduations").then(res => res.json()) as Record<string, number | undefined>;
+
+        for (const wallet of user.wallets) {
+            const count = graduates[wallet.address.toLowerCase()];
+
+            if (count && count >= inputs.count.value) {
+                return true;
+            }
+        }
+
+        return false;
+    },
+    filters: {
+        count: createFilter({
+            name: "Count",
+            required: true,
+            options: {
+                value: {
+                    name: "At least",
+                    description: "The number of traits to graduate",
+                    schema: z.number().min(1),
+                },
+            },
+        }),
+    },
+});
---
+++ apps/website/server/actions/noundry/submitTrait.ts
@@ -0,0 +1,71 @@
+import { z } from "zod";
+import { createAction } from "../createAction";
+import { createFilter } from "../createFilter";
+
+export const submitTrait = createAction({
+    image: "",
+    name: "Submit Trait",
+    category: "noundry",
+    generateDescription: async (inputs) => {
+        "use server";
+
+        const parts = [];
+
+        if (inputs.count.value > 1) {
+            parts.push({
+                text: "Submit at least",
+            });
+            parts.push({
+                text: inputs.count.value.toString(),
+                highlight: true,
+            });
+            parts.push({
+                text: "traits to",
+            });
+        } else {
+            parts.push({
+                text: "Submit a trait to",
+            });
+        }
+
+        parts.push({
+            text: "Noundry",
+            href: "https://noundry.wtf",
+        });
+
+        return parts;
+    },
+    check: async ({ user, inputs }) => {
+        "use server";
+
+        if (user.wallets.length === 0) return false;
+
+        const stats = await fetch("https://gallery.noundry.wtf/api/artists/stats").then(res => res.json()) as Array<{
+            address: string;
+            traits: number;
+        }>;
+
+        for (const wallet of user.wallets) {
+            const artist = stats.find(artist => artist.address.toLowerCase() === wallet.address.toLowerCase());
+
+            if (artist && artist.traits >= inputs.count.value) {
+                return true;
+            }
+        }
+
+        return false;
+    },
+    filters: {
+        count: createFilter({
+            name: "Count",
+            required: true,
+            options: {
+                value: {
+                    name: "At least",
+                    description: "The number of traits to submit",
+                    schema: z.number().min(1),
+                },
+            },
+        }),
+    },
+});
