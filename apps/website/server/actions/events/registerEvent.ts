5d7b9b7b089f4b24e91768515451e98f01e04430
Sam <samscolari@icloud.com>
2025-07-14 23:32:53 -0500

    fix register event

--- apps/website/server/actions/events/registerEvent.ts
+++ apps/website/server/actions/events/registerEvent.ts
@@ -7,11 +7,18 @@ import { attendees } from "~/packages/db/schema/public";
 export const registerEvent = createAction({
 	name: "registerEvent",
 	schema: z.object({
-		event: z.string().uuid().describe("The event ID to register for"),
+		event: z
+			.string()
+			.uuid()
+			.nullable()
+			.describe("The event ID to register for"),
 	}),
 	check: async ({ user, input }) => {
 		const attendee = await db.primary.query.attendees.findFirst({
-			where: and(eq(attendees.user, user.id), eq(attendees.event, input.event)),
+			where: and(
+				eq(attendees.user, user.id),
+				input.event ? eq(attendees.event, input.event) : undefined,
+			),
 		});
 
 		return !!attendee;
