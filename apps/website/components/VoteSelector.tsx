8ebd8d0294083f8b5f15a449dc1984c2da602f27
Sam <samscolari@icloud.com>
2025-07-27 15:45:27 -0500

    Update VoteSelector.tsx

--- apps/website/components/VoteSelector.tsx
+++ apps/website/components/VoteSelector.tsx
@@ -50,7 +50,7 @@ export default function VoteSelector(props: {
 		>
 			{props.ten ? (
 				<button
-					className="text-grey-200 rounded-lg text-sm border-grey-400 border-2 mr-1 p-1 hover:bg-grey-400 hover:text-white transition-colors"
+					className="text-grey-200 rounded-lg border-grey-400 border-2 mr-1 p-1 hover:bg-grey-400 hover:text-white transition-colors"
 					onClick={(e) => {
 						e.preventDefault();
 						e.stopPropagation();
